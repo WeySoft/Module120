de0d7094bb7add8a0dd6b0bcee3a882a4cb867df
Nik Wey <nik@nikwey.ch>
2021-03-04 08:42:45 +0100

    Added Abgabe Files

---
+++ Abgaben/INF19F_Wey_Nik_A3/M120Projekt/M120Projekt/APIDemo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace M120Projekt
+{
+    static class APIDemo
+    {
+        #region KlasseA
+        // Create
+        public static void DemoACreate()
+        {
+            Debug.Print("--- DemoACreate ---");
+            // ToDo
+            Data.ToDo klasseA1 = new Data.ToDo();
+            klasseA1.Titel = "ToDo 2";
+            klasseA1.MachenBis = DateTime.Today;
+            klasseA1.Abgeschlossen = false;
+            klasseA1.Beschreibung = "Blabla bla";
+            klasseA1.Widerholungen = 2;
+            klasseA1.Teamfarbe = "Rot";
+            Data.ToDo klasseA2 = new Data.ToDo();
+            klasseA2.Titel = "ToDo 3";
+            klasseA2.MachenBis = DateTime.Today;
+            klasseA2.Abgeschlossen = false;
+            klasseA2.Beschreibung = "Blabla bla";
+            klasseA2.Widerholungen = 2;
+            klasseA2.Teamfarbe = "Rot";
+            Int64 klasseA1Id = klasseA1.Erstellen();
+            Int64 klasseA2Id = klasseA2.Erstellen();
+            Debug.Print("Artikel erstellt mit Id:" + klasseA1Id);
+        }
+        public static void DemoACreateKurz()
+        {
+            Data.ToDo klasseA2 = new Data.ToDo { Titel = "Artikel 2", Abgeschlossen = true, MachenBis = DateTime.Today, Teamfarbe = "Rot", Widerholungen=3, Beschreibung="blablabla", };
+            Int64 klasseA2Id = klasseA2.Erstellen();
+            Debug.Print("Artikel erstellt mit Id:" + klasseA2Id);
+        }
+
+        // Read
+        public static void DemoARead()
+        {
+            Debug.Print("--- DemoARead ---");
+            // Demo liest alle
+            foreach (Data.ToDo klasseA in Data.ToDo.LesenAlle())
+            {
+                Debug.Print("Artikel Id:" + klasseA.ToDoId + " Name:" + klasseA.Titel);
+            }
+        }
+        // Update
+        public static void DemoAUpdate()
+        {
+            Debug.Print("--- DemoAUpdate ---");
+            // ToDo ändert Attribute
+            Data.ToDo klasseA1 = Data.ToDo.LesenID(1);
+            klasseA1.Titel = "Artikel 1 nach Update";
+            klasseA1.Aktualisieren();
+        }
+        // Delete
+        public static void DemoADelete()
+        {
+            Debug.Print("--- DemoADelete ---");
+            Data.ToDo.LesenID(5).Loeschen();
+            Debug.Print("Artikel mit Id 2 gelöscht");
+        }
+        #endregion
+    }
+}
---
+++ Abgaben/INF19F_Wey_Nik_A3/M120Projekt/M120Projekt/Data/ToDo.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace M120Projekt.Data
+{
+    public class ToDo
+    {
+        #region Datenbankschicht
+        [Key]
+        public Int64 ToDoId { get; set; }
+        [Required]
+        public String Titel { get; set; }
+        [Required]
+        public DateTime MachenBis { get; set; }
+        [Required]
+        public Boolean Abgeschlossen { get; set; }
+        [Required]
+        public Int64 Widerholungen { get; set; }
+        [Required]
+        public String Beschreibung { get; set; }
+        [Required]
+        public String Teamfarbe { get; set; }
+
+        #endregion
+        #region Applikationsschicht
+        public ToDo() { }
+        [NotMapped]
+        public String BerechnetesAttribut
+        {
+            get
+            {
+                return "Im Getter kann Code eingefügt werden für berechnete Attribute";
+            }
+        }
+        public static List<ToDo> LesenAlle()
+        {
+            using (var db = new Context())
+            {
+                return (from record in db.ToDo select record).ToList();
+            }
+        }
+        public static ToDo LesenID(Int64 klasseAId)
+        {
+            using (var db = new Context())
+            {
+                return (from record in db.ToDo where record.ToDoId == klasseAId select record).FirstOrDefault();
+            }
+        }
+        public static List<ToDo> LesenAttributGleich(String suchbegriff)
+        {
+            using (var db = new Context())
+            {
+                return (from record in db.ToDo where record.Titel == suchbegriff select record).ToList();
+            }
+        }
+        public static List<ToDo> LesenAttributWie(String suchbegriff)
+        {
+            using (var db = new Context())
+            {
+                return (from record in db.ToDo where record.Titel.Contains(suchbegriff) select record).ToList();
+            }
+        }
+        public Int64 Erstellen()
+        {
+            if (this.Titel == null || this.Titel == "") this.Titel = "leer";
+            if (this.MachenBis == null) this.MachenBis = DateTime.MinValue;
+            using (var db = new Context())
+            {
+                db.ToDo.Add(this);
+                db.SaveChanges();
+                return this.ToDoId;
+            }
+        }
+        public Int64 Aktualisieren()
+        {
+            using (var db = new Context())
+            {
+                db.Entry(this).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return this.ToDoId;
+            }
+        }
+        public void Loeschen()
+        {
+            using (var db = new Context())
+            {
+                db.Entry(this).State = System.Data.Entity.EntityState.Deleted;
+                db.SaveChanges();
+            }
+        }
+        public override string ToString()
+        {
+            return ToDoId.ToString(); // Für bessere Coded UI Test Erkennung
+        }
+        #endregion
+    }
+}
